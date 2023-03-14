47ef28da9046690c2a137e2b9436b968b3ec2f24
Himanshu Negi <himanshu.21negi@gmail.com>
2023-03-14 20:18:24 +0530

    Register Email added

--- src/components/events/singleEvent.jsx
+++ src/components/events/singleEvent.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Image from "next/image";
 
 const SingleEvent = ({ data }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
-    <div className="singleEvent">
+    <div className="singleEvent event_single_page">
       <h2>{data.title}</h2>
       <Image width="1000" height="500" src={data.image} alt={data.title} />
       <p>{data.description}</p>
-      <input type="email" placeholder="example@gmail.com" />
-      <button>Submit</button>
+      <form onSubmit={handleSubmit} className="email_registration">
+        <label>Register for the event</label>
+        <br />
+        <input type="email" id="email" placeholder="example@gmail.com" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
